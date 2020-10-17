2fec23968d5e3576966c010c49e75d5770e2c7d0
Carlo Santos <carlo@snapit.solutions>
2020-10-17 10:30:16 -0500

    starter examples

---
+++ 1_basicJs/0_helloWorld.js
@@ -0,0 +1,4 @@
+// run this by navigating to the 1_basicJs folder and 
+// running `node 0_helloWorld.js` in your terminal
+
+console.log("Hello World");
---
+++ 1_basicJs/1_creatingVariables.js
@@ -0,0 +1,6 @@
+var myFirstVariable = 1;
+let aBetterWayToCreateAVariable = 2;
+const aVarThatWillNotChange = 3.14;
+
+
+console.log();
