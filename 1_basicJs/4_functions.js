112335382bd24343db160e609ce450503baa7330
Carlo Santos <carlo@snapit.solutions>
2020-10-28 13:33:27 -0500

    adding arrow function examples

--- 1_basicJs/4_functions.js
+++ 1_basicJs/4_functions.js
@@ -2,6 +2,9 @@
 // values or arguments that will be passed into the function
 //
 // arguments are the actual values to be passed into a function
-function nameOfVariable(){
-  
+
+
+// IMPLEMENT YOUR CHANGES IN THIS FILE
+function nameOfFunction(){
+
 }
